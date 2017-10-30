8d16adeb51c85e9c6dfc34b5112f2a2055356229
wurui1994 <wurui1994@github.com>
2017-10-30 21:57:23 +0800

    Update Python Win32 Example

--- Sources/Python/Win32/fontcolor.py
+++ Sources/Python/Win32/fontcolor.py
@@ -1,7 +1,8 @@
-import win32gui, win32con, win32api
-import time, math, random
 import ctypes
+import time, math, random
 from random import randint
+import win32gui, win32con, win32api
+
 dx=10
 
 def OnPaint(hwnd, msg, wp, lp):
@@ -18,13 +19,13 @@ def OnPaint(hwnd, msg, wp, lp):
 	win32gui.SetGraphicsMode(dc, win32con.GM_ADVANCED)
 	l,t,r,b=win32gui.GetClientRect(hwnd)
 	br=win32gui.CreateSolidBrush(win32api.RGB(0,0,255))
-	bitmap=win32gui.CreateBitmap(20,5,4,1,[1,2])
+	bitmap=win32gui.CreateBitmap(20,5,4,1,None)
 	win32gui.SelectObject(dc, bitmap)
 	win32gui.SelectObject(dc, br)
 	win32gui.SelectObject(dc, hfont)
 	win32gui.SetTextColor(dc,win32api.RGB(randint(1,255),randint(1,255),randint(1,255)));
-	# win32gui.DrawText(dc,'hello',-1,(100,100,300,300),0)
-	# win32gui.FillRect(dc,(200+dx,200+dx,100+dx,100+dx),br)
+	win32gui.DrawText(dc,'hello',-1,(100,100,300,300),0)
+	win32gui.FillRect(dc,(200+dx,200+dx,100+dx,100+dx),br)
 	dx=(dx+10)%100
 	win32gui.EndPaint(hwnd, ps)
 	return 0
@@ -40,10 +41,12 @@ class_atom=win32gui.RegisterClass(wc)
 hwnd = win32gui.CreateWindow(class_atom,'hello',
 	win32con.WS_OVERLAPPEDWINDOW|win32con.WS_VISIBLE,
 	350,120,640,480, 0, 0, 0, None)
-for x in xrange(30):
+    
+for _ in range(30):
 	win32gui.InvalidateRect(hwnd,None,True)
 	win32gui.PumpWaitingMessages()
 	time.sleep(0.1)
+    
 win32gui.DestroyWindow(hwnd)
 win32gui.UnregisterClass(class_atom,None)
 
