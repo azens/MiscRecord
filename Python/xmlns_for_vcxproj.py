582074b4a6e8f8012dcf6d4f6a49d61d26701b0f
wurui1994 <wurui1994@github.com>
2017-10-30 19:16:14 +0800

    Add xmlns vcxproj exmaple

---
+++ Python/xmlns_for_vcxproj.py
@@ -0,0 +1,29 @@
+from lxml import etree
+# from lxml import objectify
+from copy import deepcopy
+import sys
+
+filename = sys.argv[1]
+
+with open(filename,'rb') as f:
+    data =  f.read()
+html = etree.XML(data)
+namespaces = {'ns':html.nsmap[None]}
+result = etree.tostring(html, pretty_print=True)
+result = html.xpath('//ns:ItemDefinitionGroup',namespaces=namespaces)
+result[1][0] = deepcopy(result[0][0])
+result[1][1] = deepcopy(result[0][1])
+
+result[1][0][2].text = result[1][0][2].text.replace('Disabled','Full')
+result[1][0][3].text = result[1][0][3].text.replace('_DEBUG','NDEBUG')
+result[1][1][3].text = result[1][1][3].text.replace('debug','release')
+
+result[1][1].append(etree.fromstring("<AdditionalOptions>/SAFESEH:NO %(AdditionalOptions)</AdditionalOptions>"))
+# html.replace(result[1][0],result[0][0])
+prefix = '<?xml version="1.0" encoding="utf-8"?>\n'
+# print(type(prefix))
+# print(type(etree.tostring(html)))
+with open(filename,'wb') as f:
+    f.write(bytes(prefix,'utf-8')+etree.tostring(html))
+    
+# for /f %f in (list.txt) do @python test.py %f
