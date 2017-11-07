0cc263d6b113c36380ebe3a4f6471b068d130963
wurui1994 <1341531859@qq.com>
2017-11-07 21:17:48 +0800

    Update Image Spider Example.

--- Python/ImageSpider.py
+++ Python/ImageSpider.py
@@ -10,9 +10,9 @@ import os
 class ImageSpider(CrawlSpider):
     name = 'imagespider'
     allowed_domains = ['www.website.com','img.website.com']
-    start_urls = ['http://www.website.com/']
+    start_urls = ['https://www.website.com/']
     rules = (
-        Rule(LinkExtractor(allow=r'http://www.website.com/'), callback='parse_item',follow=True),
+        Rule(LinkExtractor(allow=r'https://www.website.com/'), callback='parse_item',follow=True),
         # Rule(LinkExtractor(allow=r'http://img.website.com/*.jpg'), callback='parse_item', follow=False),
     )
 
@@ -21,7 +21,10 @@ class ImageSpider(CrawlSpider):
         
     def download(self,response):
         url=response.url
+        if not url.startswith('http'):return
+        
         filename=url.replace('http://','').replace('https://','')
+        filename=filename.replace(':','_').replace('?','_')
         if not filename.endswith('.html'):
             if filename.endswith('/'):filename += 'index.html'
             elif not filename.endswith('.jpg'):filename += '.html'
@@ -36,4 +39,4 @@ class ImageSpider(CrawlSpider):
     def parse_item(self, response):
         # yield self.download(response)
         for src in response.xpath('//img/@src').extract():
-            yield Request(src,callback=self.parse_image)
+            if src.startswith('http'):yield Request(src,callback=self.parse_image)
