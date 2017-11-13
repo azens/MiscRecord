d8a1d3e9c7ea0daaaff1ab17febdc6700eb22c4d
wurui1994 <1341531859@qq.com>
2017-11-13 11:12:13 +0800

    Add a maven example.

---
+++ Sources/Java/buildmvn.sh
@@ -0,0 +1,4 @@
+mvn archetype:generate -DgroupId=demo -DartifactId=Console -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false -DarchetypeCatalog=local
+cd Console
+mvn compile
+mvn exec:java -Dexec.mainClass="demo.App" -q
