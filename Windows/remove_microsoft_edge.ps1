00441e191ad219a2b5a26c2070e0cd6921fa4e1e
wurui1994 <1341531859@qq.com>
2017-11-28 10:46:49 +0800

    Fix remove_microsoft_edge script.

--- Windows/remove_microsoft_edge.ps1
+++ Windows/remove_microsoft_edge.ps1
@@ -1,10 +1,10 @@
 # powershell -c start cmd -verb runas
 # Install-Package PSSQLite
+# Import-Module PSSQLite
 # Invoke-SqliteQuery -Database "C:\ProgramData\Microsoft\Windows\AppRepository\StateRepository-Machine.srd" -Query "select PackageFullName,IsInbox from Package where PackageFullName like '%Microsoft%' "
 
 takeown /f C:\ProgramData\Microsoft\Windows\AppRepository /r /d n | out-null
 icacls C:\ProgramData\Microsoft\Windows\AppRepository /grant 'Everyone:(OI)(CI)F' /T /Q
-Import-Module PSSQLite
-Invoke-SqliteQuery -Database "C:\ProgramData\Microsoft\Windows\AppRepository\StateRepository-Machine.srd" -Query "update set IsInbox=1 from Package where PackageFullName like '%Edge%' "
+Invoke-SqliteQuery -Database "C:\ProgramData\Microsoft\Windows\AppRepository\StateRepository-Machine.srd" -Query "update Package set IsInbox=0 where PackageFullName like '%Edge%' "
 
 Get-AppxPackage *edge*|Remove-AppxPackage
