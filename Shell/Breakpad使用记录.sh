dc58f31570a2893655f3c91deb345db9a235ea90
wurui1994 <1341531859@qq.com>
2018-01-24 16:52:51 +0800

    Add Breakpad notes and git Reset all.

---
+++ Shell/Breakpad使用记录.sh
@@ -0,0 +1,16 @@
+# For macOS
+# dsymutil file -> file.dSYM
+# dump_syms file.dSYM
+xcodebuild dump_syms.xcodeproj -project
+dump_syms file > file.sym
+
+# For Windows
+src\tools\windows\refresh_binaries.bat
+dump_syms file.pdb > file.sym
+# or dump_syms file.exe > file.sym
+
+minidump_stackwalk file.dmp file.sym > info.txt 2>&1
+find "file.pdb" info.txt
+mkdir -p file.pdb/290C132D8C2C4B9E8172C148D94140361/
+copy file.sym file.pdb/290C132D8C2C4B9E8172C148D94140361/
+mindump_stackwalk
