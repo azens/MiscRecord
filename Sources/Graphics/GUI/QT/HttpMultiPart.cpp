7631841cdf05bd43b3d5e0a012e2002037818935
wurui1994 <1341531859@qq.com>
2018-01-30 12:44:36 +0800

    Update HttpMultiPart example with QPointer.

--- Sources/Graphics/GUI/QT/HttpMultiPart.cpp
+++ Sources/Graphics/GUI/QT/HttpMultiPart.cpp
@@ -4,13 +4,13 @@
 
 
 QByteArray WebRequest(QString url, int& status_code, QString method = "get",
-	QByteArray postData = "", QHttpMultiPart* HttpMultiPart=nullptr, QList<QNetworkReply::RawHeaderPair>* reply_pairs = nullptr, int time_out = 0)
+	QByteArray postData = "", QHttpMultiPart* HttpMultiPart = nullptr, QList<QNetworkReply::RawHeaderPair>* reply_pairs = nullptr, int time_out = 0)
 {
 	QNetworkAccessManager networkManager;
 	QNetworkRequest request;
 	QSslConfiguration config;
 
-	request.setRawHeader("Cookie","JSESSIONID=BAA9CEC316F39B3EE13AEB985670C669");
+	request.setRawHeader("Cookie", "JSESSIONID=BAA9CEC316F39B3EE13AEB985670C669");
 	request.setUrl(QUrl(url));
 
 	QNetworkReply* reply;
@@ -26,9 +26,9 @@ QByteArray WebRequest(QString url, int& status_code, QString method = "get",
 			HttpMultiPart->setParent(reply);
 		}
 		else
-		{ 
+		{
 			request.setHeader(QNetworkRequest::ContentTypeHeader,
-			"application/x-www-form-urlencoded");
+				"application/x-www-form-urlencoded");
 			reply = networkManager.post(request, postData);
 		}
 	}
@@ -84,15 +84,15 @@ QByteArray WebRequest(QString url, int& status_code, QString method = "get",
 
 
 /*
---boundary_.oOo._MTIzODY=MzI2NzI=MjgxMDk= 
-Content-Type: application/octet-stream 
-Content-Disposition: form-data; name=srt;filename=Zootopia.2016.1080p.BluRay.x264-SPARKS.srt 
+--boundary_.oOo._MTIzODY=MzI2NzI=MjgxMDk=
+Content-Type: application/octet-stream
+Content-Disposition: form-data; name=srt;filename=Zootopia.2016.1080p.BluRay.x264-SPARKS.srt
 ......内容省略......
---boundary_.oOo._MTIzODY=MzI2NzI=MjgxMDk= 
-Content-Disposition: form-data; name="taskId" 
- 
-143 
---boundary_.oOo._MTIzODY=MzI2NzI=MjgxMDk=-- 
+--boundary_.oOo._MTIzODY=MzI2NzI=MjgxMDk=
+Content-Disposition: form-data; name="taskId"
+
+143
+--boundary_.oOo._MTIzODY=MzI2NzI=MjgxMDk=--
 */
 
 //curl -F "srt=@Zootopia.2016.1080p.BluRay.x264-SPARKS.srt" -F "taskId=143" "http://website/upload/uploadSrt4Detail" -H "Cookie: JSESSIONID=BAA9CEC316F39B3EE13AEB985670C669" 
@@ -101,15 +101,15 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 
-	QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
+	QPointer<QHttpMultiPart> multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
 
 	QHttpPart filePart;
 	filePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/octet-stream"));
 	filePart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=srt;filename=\"Zootopia.2016.1080p.BluRay.x264-SPARKS.srt\""));
-	QFile *file = new QFile("Zootopia.2016.1080p.BluRay.x264-SPARKS.srt");
+	QPointer<QFile> file= new QFile("Zootopia.2016.1080p.BluRay.x264-SPARKS.srt");
 	file->open(QIODevice::ReadOnly);
 	filePart.setBodyDevice(file);
-	file->setParent(multiPart); // we cannot delete the file now, so delete it with the multiPart
+	file->setParent(multiPart.data()); // we cannot delete the file now, so delete it with the multiPart
 
 	QHttpPart textPart;
 	textPart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"taskId\""));
@@ -117,10 +117,10 @@ int main(int argc, char *argv[])
 
 	multiPart->append(filePart);
 	multiPart->append(textPart);
-	
+
 	QString url = "http://website/upload/uploadSrt4Detail";
 	int status_code;
-	qDebug() << WebRequest(url,status_code,"post","", multiPart);
+	qDebug() << WebRequest(url, status_code, "post", "", multiPart.data());
 	//return a.exec();
 }
 
