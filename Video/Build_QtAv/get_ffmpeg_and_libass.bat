e78e544d664bef830248e5b28bb5fd2543a9dbb2
wurui1994 <1341531859@qq.com>
2018-02-01 09:08:21 +0800

    Update get_ffmpeg_and_libass.bat

--- Video/Build_QtAv/get_ffmpeg_and_libass.bat
+++ Video/Build_QtAv/get_ffmpeg_and_libass.bat
@@ -1,8 +1,10 @@
 @echo off
-set PackageVersion=ffmpeg-20171204-71421f3-win32
+set PackageVersion=ffmpeg-3.4.1-win32
 REM bash -c "wget https://github.com/ShiftMediaProject/FFmpeg/releases/download/3.3.5.r89114/libffmpeg_3.3.5.r89114_msvc14.zip"
-bash -c "wget https://ffmpeg.zeranoe.com/builds/win32/dev/%PackageVersion%-dev.zip"
-bash -c "wget https://ffmpeg.zeranoe.com/builds/win32/shared/%PackageVersion%-shared.zip"
+bash -c "wget -c https://ffmpeg.zeranoe.com/builds/win32/dev/%PackageVersion%-dev.zip"
+bash -c "wget -c https://ffmpeg.zeranoe.com/builds/win32/shared/%PackageVersion%-shared.zip"
+bash -c "wget -c https://github.com/ShiftMediaProject/libass/releases/download/0.14.0-3/libass_0.14.0-3_msvc14.zip"
+REM goto :eof
 
 bash -c "unzip %PackageVersion%-dev.zip"
 bash -c "unzip %PackageVersion%-shared.zip"
@@ -28,28 +30,17 @@ for %%f in (*.def) do lib /def:%%f
 
 del *.exp *.def
 
-rename avcodec-58.lib avcodec.lib
-rename avdevice-58.lib avdevice.lib
-rename avfilter-7.lib avfilter.lib
-rename avformat-58.lib avformat.lib
-rename avutil-56.lib avutil.lib
-rename postproc-55.lib postproc.lib
-rename swresample-3.lib swresample.lib
-rename swscale-5.lib swscale.lib
+rename avcodec-57.lib avcodec.lib
+rename avdevice-57.lib avdevice.lib
+rename avfilter-6.lib avfilter.lib
+rename avformat-57.lib avformat.lib
+rename avutil-55.lib avutil.lib
+rename postproc-54.lib postproc.lib
+rename swresample-2.lib swresample.lib
+rename swscale-4.lib swscale.lib
 popd
 
-pushd %cd%
-cd include\libavfilter
-echo #ifndef AVFILTER_AVFILTERGRAPH_H > avfiltergraph.h
-echo #define AVFILTER_AVFILTERGRAPH_H >> avfiltergraph.h
-echo #include "avfilter.h" >> avfiltergraph.h
-echo #include "libavutil/log.h" >> avfiltergraph.h
-echo #endif /* AVFILTER_AVFILTERGRAPH_H */ >> avfiltergraph.h
-popd
-
-bash -c "wget https://github.com/ShiftMediaProject/libass/releases/download/0.14.0/libass_0.14.0_msvc14.zip"
-
-bash -c "unzip libass_0.14.0_msvc14.zip"
+bash -c "unzip libass_0.14.0-3_msvc14.zip"
 
 copy bin\x86\ass.dll bin\ass.dll
 copy lib\x86\ass.lib lib\ass.lib
