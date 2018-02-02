0d7864e96f600d18fc17585856237956be0ae21a
wurui1994 <1341531859@qq.com>
2018-02-02 15:05:40 +0800

    Add more jq example.

--- Shell/Jq_Example/getInfo.sh
+++ Shell/Jq_Example/getInfo.sh
@@ -6,3 +6,15 @@ jq '.data|{img:.img,video:.video}' videoinfo.json
 jq '.data|[.img,.video]' videoinfo.json
 jq '.data|.img,.video' videoinfo.json
 jq '.data|keys[0,2]' videoinfo.json
+jq '.data.origSubs|length' videoinfo.json
+
+# [] for each.
+jq '.data.origSubs[].content' videoinfo.json
+jq '.data.origSubs[0]|keys' videoinfo.json
+
+jq 'length' videoinfo.json
+jq 'keys' videoinfo.json
+jq '.code|type' videoinfo.json
+jq '.data|keys' videoinfo.json
+jq '.data|.["img","origSubs","video"]|type' videoinfo.json
+jq '.data[]|type' videoinfo.json
