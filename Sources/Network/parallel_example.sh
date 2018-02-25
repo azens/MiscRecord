e875503142c3a5a9323ae19316e7fa813c67295c
wurui1994 <1341531859@qq.com>
2018-02-25 14:57:18 +0800

    Add parallel example.

---
+++ Sources/Network/parallel_example.sh
@@ -0,0 +1,13 @@
+# seq 4|parallel -n0 -j4 -u "curl -I www.baidu.com"
+#
+for f in {1..37}
+do
+url="http://www.tutu12345.cc/a/cn/xiuren/list_6_$f.html"
+echo $url
+curl -s $url  | xmllint --format --html --xpath '/html/body/div[2]/div[4]/ul/li/a/@href' - 2>/dev/nulll | tr " " "\n" |sed -e 's/^href="//' -e 's/"$//' |grep http>> list.txt
+done
+#
+cat list.txt | parallel -j0 "curl -s {} | xmllint --format --html --xpath '/html/body/p[3]/a[2]/@href' - 2>/dev/null | tr ' ' '\n' |sed -e 's/^href=\"//' -e 's/\"$//'|grep http >> urls.txt"
+cat urls.txt | parallel -j0 "curl -I {} |grep Location|sed 's/Location: //g' >> files.txt"
+dos2unix files.txt
+cat files.txt | parallel --bar -u -j0 wget -c
