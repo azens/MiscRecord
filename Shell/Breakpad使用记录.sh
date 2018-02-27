c7ee90af12e1638dcec5980cdda55c381fdc625b
wurui1994 <1341531859@qq.com>
2018-02-27 17:10:18 +0800

    Update breakpad note.

--- Shell/Breakpad使用记录.sh
+++ Shell/Breakpad使用记录.sh
@@ -9,10 +9,11 @@ src\tools\windows\refresh_binaries.bat
 dump_syms file.pdb > file.sym
 # or dump_syms file.exe > file.sym
 
-minidump_stackwalk file.dmp file.sym > info.txt 2>&1
-find "file.pdb" info.txt
+minidump_stackwalk file.dmp . > info.txt 2>&1
+
 # MacOS
 # file.sym/SubtitleClient/C644C83DD49C3403BCDA79C869398B840/SubtitleClient.sym
-mkdir -p file.pdb/290C132D8C2C4B9E8172C148D94140361/
-copy file.sym file.pdb/290C132D8C2C4B9E8172C148D94140361/
-mindump_stackwalk
+s=$(grep "file.pdb|" info.txt | cut -d '|' -f 2|tr -d '\r')
+mkdir -p file.pdb/$s
+copy file.sym file.pdb/$/
+minidump_stackwalk file.dmp . > info.txt
